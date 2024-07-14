01aee039a57f761bfb5350faf5c18b30620e3a73
c-kiplimo <kiplimocollins855@gmail.com>
2024-07-14 14:10:10 +0300

    setup db connection

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+const config = {
+    mongoURI: {
+        production: 'mongodb+srv://ckiplimo54:Ngetich1234@yolo.nudp5jl.mongodb.net/darkroom?retryWrites=true&w=majority',
+        development: 'mongodb+srv://ckiplimo54:Ngetich1234@yolo.nudp5jl.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+        test: 'mongodb+srv://ckiplimo54:Ngetich1234@yolo.nudp5jl.mongodb.net/darkroom-test?retryWrites=true&w=majority'
+    }
+};
+
+module.exports = config;
--- backend/server.js
+++ backend/server.js
@@ -3,46 +3,49 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const upload = multer();
+const config = require('./config');
+const dotenv = require('dotenv');
+
+
+dotenv.config(); 
 
 const productRoute = require('./routes/api/productRoute');
 
-// Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+const env = process.env.NODE_ENV || 'development';
+const mongodb_url = config.mongoURI[env];
+const MONGODB_URI = process.env.MONGODB_URI || mongodb_url;
 
-// define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
 
 // Check Connection
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+db.once('open', () => {
+    console.log('Database connected successfully');
+});
 
 // Check for DB Errors
-db.on('error', (error)=>{
+db.on('error', (error) => {
     console.log(error);
-})
+});
 
 // Initializing express
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(express.json())
+app.use(express.json());
 
-// 
+// Multer middleware
 app.use(upload.array()); 
 
-// Cors 
+// CORS middleware
 app.use(cors());
 
 // Use Route
-app.use('/api/products', productRoute)
+app.use('/api/products', productRoute);
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
